a997a2c59153d3ffcc413c26c6f254f9f7760ce3
tayru <denis.brudanin@mail.ru>
2023-03-25 11:56:07 +0300

    counter

--- src/pages/counter/counter.jsx
+++ src/pages/counter/counter.jsx
@@ -1,7 +1,19 @@
+import './index.scss';
+import React from 'react'
+export function CounterPage() {
+   const [count, setCount] = React.useState(0);
+   const  onClickPlus = () => {
+         setCount(count + 1)
+     };
+    const  onClickMinus = () => {
+        setCount(count - 1)
+    };
 
-export const CounterPage = () => (
-    <>
-        <h3>Counter</h3>
-
-    </>
-);
+    return(
+    <div className="Counter">
+            <h2>Счетчик:</h2>
+            <h1>{count}</h1>
+            <button onClick={onClickMinus} className="minus">- Минус</button>
+            <button onClick={onClickPlus} className="plus">Плюс +</button>
+    </div>
+)}
