6d51823b401134e99bc5f91b1c3fe498a4e9a558
tayru <denis.brudanin@mail.ru>
2023-03-30 21:40:10 +0300

    add import react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MainPage } from './pages/main';
 import { CounterPage } from './pages/counter';
